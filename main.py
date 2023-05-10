22d7ea00d4c073524ff6f33fbe1a40e87310771b
Blake <blake@uconn.edu>
2023-05-10 08:28:40 -0400

    Updates

--- main.py
+++ main.py
@@ -49,30 +49,51 @@ print(f'Number of test graphs: {len(test_dataset)}')
 train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
 
-model = GCN(in_channels=dataset.num_node_features, hidden_channels=64, out_channels=dataset.num_classes)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-criterion = torch.nn.CrossEntropyLoss()
-print(model)
+# model = GCN(in_channels=dataset.num_node_features, hidden_channels=64, out_channels=dataset.num_classes)
+# optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+# criterion = torch.nn.CrossEntropyLoss()
+# print(model)
+# pbar = tqdm(range(1,100))
+# for epoch in pbar:
+#     train(model, train_loader, optimizer, criterion)
+#     train_acc = test(model, train_loader)
+#     test_acc = test(model, test_loader)
+#     pbar.set_postfix_str(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
 
-pbar = tqdm(range(1,100))
-for epoch in pbar:
-    train(model, train_loader, optimizer, criterion)
-    train_acc = test(model, train_loader)
-    test_acc = test(model, test_loader)
-    pbar.set_postfix_str(f'Epoch: {epoch:03d}, Train Acc: {train_acc:.4f}, Test Acc: {test_acc:.4f}')
+# torch.save(model, "MUTAG_model.pth")
+
+model = torch.load("MUTAG_model.pth")
 
 init_graph = dataset[0]
-target_class = 0
+target_class = 1
 
 interpreter = GNNInterpreter(model.get_embedding_outputs, train_dataset)
 print(init_graph.y)
 pg = interpreter.train(dataset[0], target_class)
 
-explanation_graphs = pg.sample_explanations(1)
+atom_indices = {
+  0: "C",
+  1: "N",
+  2: "O",
+  3: "F",
+  4: "I",
+  5: "Cl",
+  6: "Br",
+}
+
+labels = dict(zip(range(pg.Xi.shape[0]), map(atom_indices.get, torch.argmax(init_graph.x, dim=1).detach().numpy())))
+nx.draw_networkx(to_networkx(init_graph, to_undirected=True), with_labels=True, node_color = torch.argmax(init_graph.x, dim=1), labels=labels)
+plt.savefig(f"explanations/Original Graph.png")
+
+explanation_graphs = pg.sample_explanations(5)
 print("Example Outputs:\n", torch.softmax(interpreter.get_embedding_outputs(explanation_graphs)[1], dim=1).detach())
 for i, data in enumerate(explanation_graphs.to_data_list()):
     G = to_networkx(data, to_undirected=True)
     # G.remove_edges_from(nx.selfloop_edges(G))
-    nx.draw_networkx(G, with_labels=False)
+    print(torch.argmax(pg.Xi, dim=1))
+    labels = dict(zip(range(pg.Xi.shape[0]), map(atom_indices.get, torch.argmax(pg.Xi, dim=1).detach().numpy())))
+    print(labels)
+    nx.draw_networkx(G, with_labels=True, node_color = torch.argmax(pg.Xi, dim=1), labels=labels)
     print("Node features equal:", torch.equal(torch.round(pg.Xi), init_graph.x))
     plt.savefig(f"explanations/Target Class {target_class} Explanation Graph {i}.png")
+    plt.cla()
